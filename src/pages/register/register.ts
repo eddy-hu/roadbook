b04a8e7dc6e116484e656302b44a1a314ea3b66b
eddy-hu <eddy.hu@nokia.com>
2019-04-25 11:24:50 -0400

    add register component

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,5 +1,13 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams, ViewController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import {
+  NavController,
+  NavParams,
+  ViewController,
+  LoadingController,
+  ToastController
+} from "ionic-angular";
+import { BaseUI } from "../../common/baseui";
+import { RestProvider } from "../../providers/rest/rest";
 
 /**
  * Generated class for the RegisterPage page.
@@ -9,23 +17,66 @@ import { NavController, NavParams, ViewController } from 'ionic-angular';
  */
 
 @Component({
-  selector: 'page-register',
-  templateUrl: 'register.html',
+  selector: "page-register",
+  templateUrl: "register.html"
 })
-export class RegisterPage {
+export class RegisterPage extends BaseUI {
+  mobile: any;
+  nickname: any;
+  password: any;
+  confirmPassword: any;
+  errorMessage: any;
 
   constructor(
     public viewCtrl: ViewController,
-    public navCtrl: NavController, 
-    public navParams: NavParams) {
+    public loadingCtrl: LoadingController,
+    public navCtrl: NavController,
+    public rest: RestProvider,
+    public toastCtrl: ToastController,
+    public navParams: NavParams
+  ) {
+    super();
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad RegisterPage');
+    console.log("ionViewDidLoad RegisterPage");
   }
 
-  dismiss(){
+  dismiss() {
     this.viewCtrl.dismiss();
   }
 
+  gotoLogin() {
+    this.navCtrl.pop();
+  }
+
+  doRegister() {
+    //form validation
+    if (this.mobile.length != 10) {
+      super.showToast(this.toastCtrl, "Phone number is invalid");
+    } else if (this.nickname.length < 3 || this.nickname.length > 10) {
+      super.showToast(this.toastCtrl, "Nick name is invalid");
+    } else if (this.password.length < 6 || this.password.length > 20) {
+      super.showToast(this.toastCtrl, "Password is invalid");
+    } else if (this.confirmPassword.length < 6 || this.confirmPassword.length > 20 ) {
+      super.showToast(this.toastCtrl, "Confirm Password is invalid");
+    } else if (this.password != this.confirmPassword) {
+      super.showToast(this.toastCtrl, "Passwords do not match");
+    } else {
+      var loading = super.showLoading(this.loadingCtrl, "Loading");
+      this.rest.register(this.mobile, this.nickname, this.password).subscribe(
+        f => {
+          if (f["Status"] == "OK") {
+            super.showToast(this.toastCtrl, "Sign up successfully");
+            loading.dismiss();
+            this.dismiss();
+          } else {
+            loading.dismiss();
+            super.showToast(this.toastCtrl, f["StatusContent"]);
+          }
+        },
+        error => (this.errorMessage = <any>error)
+      );
+    }
+  }
 }
--- src/providers/rest/rest.ts
+++ src/providers/rest/rest.ts
@@ -1,8 +1,8 @@
-import { Observable } from 'rxjs/Rx';
-import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/catch';
+import { Observable } from "rxjs/Rx";
+import { Injectable } from "@angular/core";
+import { Http, Response } from "@angular/http";
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/catch";
 
 /*
   Generated class for the RestProvider provider.
@@ -11,72 +11,98 @@ import 'rxjs/add/operator/catch';
 */
 @Injectable()
 export class RestProvider {
-
   constructor(public http: Http) {
     //console.log('Hello RestProvider Provider');
   }
 
   //feed
-  private apiUrlFeeds = 'https://imoocqa.gugujiankong.com/api/feeds/get';
+  private apiUrlFeeds = "https://imoocqa.gugujiankong.com/api/feeds/get";
 
   //account
-  private apiUrlRegister = 'https://imoocqa.gugujiankong.com/api/account/register';
-  private apiUrlLogin = 'https://imoocqa.gugujiankong.com/api/account/login';
-  private apiUrlLoginWithMd5 = 'https://imoocqa.gugujiankong.com/api/account/loginwithmd5';
-  private apiUrlUserInfo = 'https://imoocqa.gugujiankong.com/api/account/userinfo';
-  private apiUrlUpdateNickName = 'https://imoocqa.gugujiankong.com/api/account/updatenickname';
+  private apiUrlRegister =
+    "https://imoocqa.gugujiankong.com/api/account/register";
+  private apiUrlLogin = "https://imoocqa.gugujiankong.com/api/account/login";
+  private apiUrlLoginWithMd5 =
+    "https://imoocqa.gugujiankong.com/api/account/loginwithmd5";
+  private apiUrlUserInfo =
+    "https://imoocqa.gugujiankong.com/api/account/userinfo";
+  private apiUrlUpdateNickName =
+    "https://imoocqa.gugujiankong.com/api/account/updatenickname";
   //question
-  private apiUrlQuestionSave = 'https://imoocqa.gugujiankong.com/api/question/save';
-  private apiUrlQuestionList = 'https://imoocqa.gugujiankong.com/api/question/list?index=1&number=10';
-  private apiUrlGetQuestion = "https://imoocqa.gugujiankong.com/api/question/get";
+  private apiUrlQuestionSave =
+    "https://imoocqa.gugujiankong.com/api/question/save";
+  private apiUrlQuestionList =
+    "https://imoocqa.gugujiankong.com/api/question/list?index=1&number=10";
+  private apiUrlGetQuestion =
+    "https://imoocqa.gugujiankong.com/api/question/get";
   private apiUrlAnswer = "https://imoocqa.gugujiankong.com/api/question/answer";
 
   /**
-   * 
-   * 
-   * @param {any} mobile 
-   * @param {any} password 
-   * @returns {Observable<string[]>} 
+   *
+   *
+   * @param {any} mobile
+   * @param {any} password
+   * @returns {Observable<string[]>}
    * @memberof RestProvider
    */
   login(mobile, password): Observable<string[]> {
-    return this.getUrlReturn(this.apiUrlLogin + "?mobile=" + mobile + "&password=" + password);
+    return this.getUrlReturn(
+      this.apiUrlLogin + "?mobile=" + mobile + "&password=" + password
+    );
+  }
+/**
+ * 
+ * @param mobile 
+ * @param nickname 
+ * @param password 
+ */
+  register(mobile, nickname, password): Observable<string[]> {
+    return this.getUrlReturn(
+      this.apiUrlRegister +
+        "?mobile=" +
+        mobile +
+        "&nickname=" +
+        nickname +
+        "&password=" +
+        password
+    );
   }
 
-
   /**
-   * 
-   * 
-   * @param {any} mobile 
-   * @param {any} password 
-   * @returns {Observable<string[]>} 
+   *
+   *
+   * @param {any} mobile
+   * @param {any} password
+   * @returns {Observable<string[]>}
    * @memberof RestProvider
    */
   loginWithMd5(mobile, password): Observable<string[]> {
-    return this.getUrlReturn(this.apiUrlLoginWithMd5 + "?mobile=" + mobile + "&password=" + password);
+    return this.getUrlReturn(
+      this.apiUrlLoginWithMd5 + "?mobile=" + mobile + "&password=" + password
+    );
   }
 
   /**
-   * 
+   *
    * @Parry
    * @private
-   * @param {string} url 
-   * @returns {Observable<string[]>} 
+   * @param {string} url
+   * @returns {Observable<string[]>}
    * @memberof RestProvider
    */
   private getUrlReturn(url: string): Observable<string[]> {
-    return this.http.get(url)
+    return this.http
+      .get(url)
       .map(this.extractData)
       .catch(this.handleError);
   }
 
-
   /**
    * return json result
-   * 
+   *
    * @private
-   * @param {Response} res 
-   * @returns 
+   * @param {Response} res
+   * @returns
    * @memberof RestProvider
    */
   private extractData(res: Response) {
@@ -84,21 +110,20 @@ export class RestProvider {
     return JSON.parse(body) || {};
   }
 
-
   /**
    * exception handle
-   * 
+   *
    * @private
-   * @param {(Response | any)} error 
-   * @returns 
+   * @param {(Response | any)} error
+   * @returns
    * @memberof RestProvider
    */
   private handleError(error: Response | any) {
     let errMsg: string;
     if (error instanceof Response) {
-      const body = error.json() || '';
+      const body = error.json() || "";
       const err = body.error || JSON.stringify(body);
-      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+      errMsg = `${error.status} - ${error.statusText || ""} ${err}`;
     } else {
       errMsg = error.message ? error.message : error.toString();
     }
